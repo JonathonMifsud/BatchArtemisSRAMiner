a0d8ab419605314215b9893a9356799f42c65107
JonathonMifsud <jonathon.mifsud1@students.mq.edu.au>
2023-10-08 14:07:23 +1100

    add enviro and adapters to setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+###############################################################################################################
+#                                            BatchArtemisSRAMiner                                             #   
+#                                                JCO Mifsud                                                   # 
+#                                                   2023                                                      # 
+###############################################################################################################
 
 # This script sets up a project with a specified structure in the provided root directory.
 # It also moves all files from the current directory to the project's script directory 
@@ -20,13 +25,15 @@ scratch_dir="/scratch/${root}/${project}"
 # Create project directories in /project and /scratch
 # The -p option creates parent directories as needed and doesn't throw an error if the directory already exists.
 echo "Creating project directories..."
-mkdir -p "${project_dir}"/{scripts,accession_lists,adapters,logs,ccmetagen,blast_results,annotation,mapping,contigs/{final_logs,final_contigs},fastqc,read_count}
+mkdir -p "${project_dir}"/{scripts,accession_lists,adapters,logs,environments,ccmetagen,blast_results,annotation,mapping,contigs/{final_logs,final_contigs},fastqc,read_count}
 mkdir -p "${scratch_dir}"/{abundance,read_count,raw_reads,trimmed_reads}
 mkdir -p "${scratch_dir}"/abundance/final_abundance
 
 # Move all files from the current directory to the project's scripts directory
 echo "Moving files to the project's scripts directory..."
 mv ./* "${project_dir}/scripts"
+mv ../environment/* "${project_dir}/environments/"
+mv ../adapters/* "${project_dir}/adapters/"
 
 # Navigate to the project's scripts directory
 cd "${project_dir}/scripts"
